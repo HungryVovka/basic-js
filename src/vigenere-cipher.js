093558d4da8045f6e300e47387799d189618730e
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-10-18 02:56:23 +0500

    feat: vigenere-cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,14 +19,61 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
+
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(method = "true") {
+    this.type = method;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    let abc = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    if (!message || !key) { throw new Error("Incorrect arguments!") }
+    let keyFit = key;
+    if (keyFit.length !== message.length) {
+      while (keyFit.length < message.length) {
+        keyFit = keyFit + key;
+      }
+      keyFit = keyFit.slice(0, message.length)
+    }
+    let answer = [];
+    let j = 0;
+    for (let i = 0; i < message.length; i++){
+      message = message.toUpperCase();
+      keyFit = keyFit.toUpperCase();
+      if (abc.includes(message[i])) {
+        let key = (abc.indexOf(message[i]) + abc.indexOf(keyFit[j])) % 26;
+        answer.push(abc[key])
+        j = j + 1;
+      } else {
+        answer.push(message[i]);
+      }
+    }
+    return this.type ? answer.join("") : answer.reverse().join("");
+  }
+  
+  decrypt(message, key) {
+    if (!message || !key) { throw new Error("Incorrect arguments!") }
+    let abc = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    let keyFit = key;
+    if (keyFit.length !== message.length){
+      while (keyFit.length < message.length){
+        keyFit = keyFit + key;
+      }
+      keyFit = keyFit.slice(0, message.length);
+    }
+    let answer = [];
+    let j = 0;
+    for (let i = 0; i < message.length; i++) {
+      message = message.toUpperCase();
+      keyFit = keyFit.toUpperCase();
+      if (abc.includes(message[i])){
+        let key = (abc.indexOf(message[i]) - abc.indexOf(keyFit[j]) + 26) % 26;
+        res.push(abc[key])
+        j = j + 1
+      } else {
+        answer.push(message[i]);
+      }
+    }
+    return this.type ? answer.join("") : answer.reverse().join("");
   }
 }
 
