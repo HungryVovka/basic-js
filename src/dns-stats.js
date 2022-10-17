51929eeec866266dfbc0ee92b67d67b782e3d634
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-10-18 01:49:27 +0500

    feat: dns-stats solution

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function getDNSStats(d) {
+  var answer = {};
+  d.forEach(i => {
+    var arr = i.split(".").reverse();
+    let key = ""
+    arr.forEach(j => {
+      key = key + "." + j;
+      answer.hasOwnProperty(key) ? answer[key]++ : answer[key] = 1;
+    })
+  })
+  return answer;
 }
 
 module.exports = {
